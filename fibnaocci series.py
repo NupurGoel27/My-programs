aa8bd6ee5852570afabe19e81696c2c367fd1617
Nupur <phur143@gmail.com>
2019-09-27 11:26:36 -0700

    fib

--- fibnaocci series.py
+++ fibnaocci series.py
@@ -1,1 +1,20 @@
 
+def fib(n):
+    a=0
+    b=1
+    if n==1:
+        print(n)
+    elif n<1:
+        print("NOt a valid number. Please enter positive number: ")
+    else:
+        print(a)
+        print(b)
+
+        for i in range(2,n):
+            c=a+b
+            a=b
+            b=c
+            if c>100:
+                break
+            print(c)
+fib(100)
